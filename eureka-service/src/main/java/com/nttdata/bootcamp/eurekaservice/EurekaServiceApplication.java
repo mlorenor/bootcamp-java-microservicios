1dc2d89e08ed0528c0e1311778760021d22dfaa8
enelint\es74917543b <EndesaEnel01#>
2022-10-24 21:46:08 +0200

    eureka-service

---
+++ eureka-service/src/main/java/com/nttdata/bootcamp/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
